1224b13be28e30ccac61289d36f34ddeabe4511c
LEO\leog4 <leog4za@gmail.com>
2022-02-15 10:01:27 -0600

    making server changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS, cross_origin
 
 from resources.blog import Blog, BlogList
 
-app = Flask(__name__, static_folder='my-app/build')
+app = Flask(__name__, static_folder='my-app/build', static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://postgres:Stevenash1!@localhost/flask_blog')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -18,15 +18,10 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-@cross_origin
-def serve(path):
-     path_dir = os.path.abspath("../build") #path react build
-     if path != "" and os.path.exists(os.path.join(path_dir, path)):
-         return send_from_directory(os.path.join(path_dir), path)
-     else:
-         return send_from_directory(os.path.join(path_dir),'index.html')
+@app.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 api.add_resource(Blog, '/api/blog/<string:name>') # router configuration for get, create, update, and delete a blog | must conating a string
 api.add_resource(BlogList, '/api/blog') # router configuration for getting all the blogs that we currently have
