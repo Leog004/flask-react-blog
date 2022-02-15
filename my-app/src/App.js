808bb189ebb254964859b9ce0f05f5caceafd44f
LEO\leog4 <leog4za@gmail.com>
2022-02-14 20:23:13 -0600

    adding to heroku

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Deploy from './Components/Deploy';
+import {useState, useEffect} from 'react'
+
+
+function App() {
+
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    fetch('/api').then(response => {
+      if(response.status === 200){
+        return response.json()
+      } 
+    }).then(data => setState(data))
+    .then(error => console.log(error))
+  },[])
+
+  return (
+    <div className="App">
+      <Deploy prop={state} />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Deploy.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Deploy({prop}) {
+  return (
+    <div>{prop.tutorial}</div>
+  )
+}
