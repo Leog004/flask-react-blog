4c1320d845fc2d46f48a96bb2bc273011bfb7af6
LEO\leog4 <leog4za@gmail.com>
2022-02-15 11:36:40 -0600

    mobile optimization

--- my-app/src/Components/BlogItem.jsx
+++ my-app/src/Components/BlogItem.jsx
@@ -41,10 +41,10 @@ export default function BlogItem ({data, deleteHandle, editHandle}){
         )
     }else{
         return (
-            <div className="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
-                <div className="w-full block h-full">
-                    <div className="bg-white w-full p-4">
-                        <h2 className="text-lg font-semibold text-center">Edit {data.title}</h2>
+        <div className="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
+            <div className="w-full block h-full">
+                <div className="bg-white w-full p-4">
+                    <h2 className="text-lg font-semibold text-center">Edit {data.title}</h2>
                     <form action="#">
                         <div className="flex flex-col max-w-md">
                             <div className="p-2 md:p-6 mt-8">         
@@ -56,33 +56,32 @@ export default function BlogItem ({data, deleteHandle, editHandle}){
                                     </div>
                                     <div className="flex flex-col mb-4">
                                         <div className="relative">
-                                        <label htmlFor="title" className="text-sm font-semibold">Text</label>
+                                        <label htmlFor="text" className="text-sm font-semibold">Text</label>
                                             <input onChange={(e) => setText(e.target.value)} value={text} type="text" name="text" id="text" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
                                         </div>
-                                        <div className="flex flex-col mb-4">
-                                        <label htmlFor="title" className="text-sm font-semibold">ImageUrl</label>
-                                            <input onChange={(e) => setImageUrl(e.target.value)} value={imageUrl} type="text" name="imageUrl" id="imageUrl" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
-                                        </div>
                                     </div>
-                                </div>
+                                    <div className="flex flex-col">
+                                    <label htmlFor="imageUrl" className="text-sm font-semibold">ImageUrl</label>
+                                        <input onChange={(e) => setImageUrl(e.target.value)} value={imageUrl} type="text" name="imageUrl" id="imageUrl" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
+                                    </div>
                             </div>
-
-                            <div className="flex justify-between mt-10">
-                                <button onClick={() => {
-                                    editHandle([{slug: data.slug, title, text, imageUrl}]);
-                                    HandleForm();
-                                    }
-                                    } type="button" className="py-2 px-4  bg-blue-500 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                        Save
-                                </button>
-                                <button onClick={HandleForm} type="button" className="py-2 px-4  bg-red-500 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                    Cancel
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div className="flex justify-between mt-10">
+                            <button onClick={() => {
+                                editHandle([{slug: data.slug, title, text, imageUrl}]);
+                                HandleForm();
+                                }
+                                } type="button" className="py-2 px-4  bg-blue-500 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                    Save
+                            </button>
+                            <button onClick={HandleForm} type="button" className="py-2 px-4  bg-red-500 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
+        </div>
         )
     }
 }
